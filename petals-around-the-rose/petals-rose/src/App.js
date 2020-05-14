3e568af55f437d8e3a76c5b9792797ed5be7f8a0
andrew_killem <andrew.upk@gmail.com>
2020-05-14 17:21:53 +0300

    Randomly generated roses

--- petals-around-the-rose/petals-rose/src/App.js
+++ petals-around-the-rose/petals-rose/src/App.js
@@ -3,10 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {	
-	const values = [1, 2, 3, 4, 5, 6];
-	const facets = values.map((val, i) => <Facet value={val} key={i}/>);
-	//console.log(facets);
-	
+	const values = Array(6).fill().map(() => Math.round(Math.random() * 5+1));
+	const roses = [[' ',' ',' ',' ','*',' ',' ',' ',' '],	//1
+					 ['*',' ',' ',' ',' ',' ',' ',' ','*'],	//2
+					 ['*',' ',' ',' ','*',' ',' ',' ','*'],	//3
+					 ['*',' ','*',' ',' ',' ','*',' ','*'],	//4
+					 ['*',' ','*',' ','*',' ','*',' ','*'],	//5
+					 ['*',' ','*','*',' ','*','*',' ','*']]	//6
+	const facets = values.map((val, i) => {
+		return <Facet value={val} key={i} rose={roses[val-1]}/>
+	});
+
 	return (
 		<div className="App">
 			{facets}
@@ -17,34 +24,25 @@ function App() {
 class Facet extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {
-			roses:
-				[[' ',' ',' ',' ','*',' ',' ',' ',' '],	//1
-				 ['*',' ',' ',' ',' ',' ',' ',' ','*'],	//2
-				 ['*',' ',' ',' ','*',' ',' ',' ','*'],	//3
-				 ['*',' ','*',' ',' ',' ','*',' ','*'],	//4
-				 ['*',' ','*',' ','*',' ','*',' ','*'],	//5
-				 ['*',' ','*','*',' ','*','*',' ','*']]	//6
-			};
 	}
 	
 	render(){
 		return (
 			<div className='facet' value={this.props.value}>
 				<div className="row rowTop">
-					<div className="cell">{this.state.roses[this.props.value-1][0]}</div>
-					<div className="cell">{this.state.roses[this.props.value-1][1]}</div>
-					<div className="cell">{this.state.roses[this.props.value-1][2]}</div>
+					<div className="cell">{this.props.rose[0]}</div>
+					<div className="cell">{this.props.rose[1]}</div>
+					<div className="cell">{this.props.rose[2]}</div>
 				</div>
 				<div className="row rowMiddle">
-					<div className="cell">{this.state.roses[this.props.value-1][3]}</div>
-					<div className="cell">{this.state.roses[this.props.value-1][4]}</div>
-					<div className="cell">{this.state.roses[this.props.value-1][5]}</div>
+					<div className="cell">{this.props.rose[3]}</div>
+					<div className="cell">{this.props.rose[4]}</div>
+					<div className="cell">{this.props.rose[5]}</div>
 				</div>
 				<div className="row rowBottom">
-					<div className="cell">{this.state.roses[this.props.value-1][6]}</div>
-					<div className="cell">{this.state.roses[this.props.value-1][7]}</div>
-					<div className="cell">{this.state.roses[this.props.value-1][8]}</div>
+					<div className="cell">{this.props.rose[6]}</div>
+					<div className="cell">{this.props.rose[7]}</div>
+					<div className="cell">{this.props.rose[8]}</div>
 				</div>
 			</div>
 		);
